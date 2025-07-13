5a51d5aa45cd9d10f43b812841bee07308c4a3ff
Sami-Saqr <140464418+Sami-Saqr@users.noreply.github.com>
2025-07-13 23:19:54 +0300

    Update Swap.cpp

--- Swap.cpp
+++ Swap.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+using namespace std;
 
+void Swap(int &x, int &y) {
+    int temp = x;
+    x = y;
+    y = temp;
+}
+
+int main() {
+    int X, Y;
+    cin >> X >> Y;
+
+    Swap(X, Y);
+
+    cout << X << " " << Y << endl;
+
+    return 0;
+}
